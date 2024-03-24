8155f75042d7978bd34d31acac18c2fd807f9669
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-24 17:32:56 -0500

    Utility classes for styling text and buttons

---
+++ app/utils.js
@@ -0,0 +1,14 @@
+export const tealGradient =
+	'text-transparent bg-clip-text bg-gradient-to-br from-teal-500 via-cyan-400 to-sky-500 tracking-wide'
+
+export const pinkGradient =
+	'text-transparent bg-clip-text bg-gradient-to-br from-pink-400 via-rose-400 to-rose-500 tracking-wide'
+
+export const pinkGradientButton =
+	'text-black bg-gradient-to-br from-pink-400 via-rose-400 to-rose-500 px-6 py-3 w-full sm:w-fit rounded-full mr-4 font-semibold uppercase tracking-wider hover:from-teal-500 hover:via-cyan-400 hover:to-sky-500 transition ease-in-out'
+
+export const blackGradientButton =
+	'px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-rose-500 via-red-400 to-rose-300 hover:from-teal-500 hover:via-cyan-500 hover:to-sky-500  text-rose-300 hover:text-teal-300 mt-3 font-semibold uppercase tracking-wider transition ease-in-out'
+
+export const blackGradientButtonText =
+	'block bg-[#121212] rounded-full px-5 py-2'

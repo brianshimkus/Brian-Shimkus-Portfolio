ef287dde34ee26c9feae9a394a7df8627a953cfc
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-25 00:20:51 -0500

    Fun facts section

--- app/components/FunFactsSection.jsx
+++ app/components/FunFactsSection.jsx
@@ -1,9 +1,45 @@
+import Image from 'next/image'
 import SectionTitle from './SectionTitle'
+
+const funFacts = [
+	{
+		title: 'Guitar Player and Singer',
+		thumbnail: '/images/guitar.jpg',
+		description: 'Performing live since 2014',
+	},
+	{
+		title: 'Mixed Martial Artist',
+		thumbnail: '/images/bjj.jpg',
+		description: 'Member of Renzo Gracie Academy',
+	},
+	{
+		title: '4-time Softball Champion',
+		thumbnail: '/images/softball.jpg',
+		description: 'Member of the Duck Boys',
+	},
+]
 
 export default function FunFactsSection() {
 	return (
 		<section id='#funfacts'>
 			<SectionTitle title='Fun Facts' />
+			<div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8'>
+				{funFacts.map((funFact, index) => (
+					<div
+						key={index}
+						className='bg-zinc-800 border-2 border-zinc-700 rounded-md  transition ease-in-out duration-300 h-full px-4 py-6'>
+						<Image
+							src={funFact.thumbnail}
+							alt={funFact.title}
+							className='rounded-md mb-4'
+							width={600}
+							height={600}
+						/>
+						<h3 className='text-xl font-bold mb-2'>{funFact.title}</h3>
+						<p className='text-zinc-400'>{funFact.description}</p>
+					</div>
+				))}
+			</div>
 		</section>
 	)
 }
--- app/components/SectionTitle.jsx
+++ app/components/SectionTitle.jsx
@@ -2,7 +2,7 @@ import { tealGradient } from '../utils'
 
 export default function SectionTitle({ title }) {
 	return (
-		<div className='mt-32 mb-12'>
+		<div className='mt-32 mb-8 md:mb-12'>
 			<h2
 				className={`${tealGradient} text-3xl md:text-4xl mb-4 font-extrabold`}>
 				{title}
